07ce1cac27202751c382f683d5f76e50346c5a37
ToddSing <kty5876@aol.com>
2019-09-26 11:25:26 -0400

    Post is listing most recent

---
+++ src/scripts/post.js
@@ -0,0 +1,21 @@
+import api from "../helpers/API.js"
+
+api.listAllPosts()
+   .then((data) => {
+    let posts    
+    if(data.length > 10)
+    {
+        posts = data.slice(data.length - 10, data.length)
+    }
+    else{
+        posts = data
+    }
+     console.log(posts)
+    for( let i = posts.length - 1; i >=0; i-- )
+     {
+        let post = document.createElement("li")
+        post.textContent = `${posts[i].title}`
+        console.log(post[i])
+        document.getElementById("postsList").appendChild(post)
+     }
+})
