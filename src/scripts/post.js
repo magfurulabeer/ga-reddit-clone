064739f653720e8791da940de621670cd305d416
magfurulabeer <magfurulabeer@gmail.com>
2019-09-26 15:03:48 -0400

    Create clickable post previews

--- src/scripts/post.js
+++ src/scripts/post.js
@@ -13,8 +13,26 @@ api.listAllPosts()
      console.log(posts)
     for( let i = posts.length - 1; i >=0; i-- )
      {
-        let post = document.createElement("li")
-        post.textContent = `${posts[i].title}`
-        document.getElementById("postsList").appendChild(post)
+        console.log(posts[i])
+
+        let post = document.createElement("a")
+
+        let title = document.createElement("h3")
+        let description = document.createElement("p")
+
+        // Change this to go to post.html
+        post.setAttribute('href', '#')
+        post.classList.add('post-container')
+        title.classList.add('post-title')
+        description.classList.add('post-description')
+        
+        title.textContent = `${posts[i].title}`
+        description.textContent = `${posts[i].description}`
+        
+        post.appendChild(title)
+        post.appendChild(description)
+
+        // post.textContent = `${posts[i].title}`
+        document.getElementById("postContainer").appendChild(post)
      }
 })
