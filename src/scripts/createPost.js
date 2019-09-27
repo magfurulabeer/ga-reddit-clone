eddc571cfb02231192fb51455364a1ef813942e1
ToddSing <kty5876@aol.com>
2019-09-27 13:58:35 -0400

    styling for createPost.html

--- src/scripts/createPost.js
+++ src/scripts/createPost.js
@@ -2,13 +2,19 @@ import api from "../helpers/API.js"
 const createPost = document.getElementById("userPost")
     createPost.addEventListener("submit",create)
 
-function create(event)
+let userName = sessionStorage.getItem("username")
+document.getElementsByTagName("h1")[0].textContent = userName
+console.log(userName)
+
+    function create(event)
 {
     event.preventDefault()
     let title = document.getElementById("title").value
     let discription = document.getElementById("discription").value
     api.createPost(title,discription)
     .then((data) => {
+        
+
         // window.location.href= "../index.html"
     })
     
